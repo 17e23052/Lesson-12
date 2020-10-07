b060202adc5689bdcedf330458268e68b75b5c03
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-07 08:45:34 +0000

    Guess the number v1.4: Higher or lower (with guess limit)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 guesses = 0
 number = randint(1,10)
 print("Guess a number between 1 and 10:")
@@ -9,6 +10,9 @@ while guess != number:
     print("Go higher.")
   elif guess > number:
     print("Go lower.")
+  if guesses > 1:
+    print("You failed! Better luck next time!")
+    sleep(999)
   print("Guess a number between 1 and 10:")
   guesses = guesses + 1
   guess = int(input())
