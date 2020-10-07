8504be63b4e14b1d60376a3b495e98d971c068fd
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-07 08:29:35 +0000

    Guess the number v1.0

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-print("Guess the word")
-word = input()
-while word != "Raspberry":
-  print("Try again")
-  word = input()
-print(f"Well done, the word was {word}!")
+guesses = 0
+number = 4
+print("Guess a number between 1 and 10:")
+guess = int(input())
+while guess != 4:
+  print("Incorrect.")
+  print("Guess a number between 1 and 10:")
+  guesses = guesses + 1
+  guess = int(input())
+print("Well done!")
+guesses = guesses + 1
+print(f"You guessed it in {guesses} attempts!")
